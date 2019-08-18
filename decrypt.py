43e5c3e2c53e382f94044d90b8d82a8d88bac3d9
SDM <37592985+SDMDeveloperSec@users.noreply.github.com>
2019-08-18 22:40:10 +0000

    Create decrypt.py

---
+++ decrypt.py
@@ -0,0 +1,18 @@
+import crypt
+import os
+
+
+class main() :
+	salt = input("Insira o Salt     :")
+	hash = input("Insira o Hash     :")
+	path = input("Insira a WordList :")
+	with open(path, 'r') as file :
+		for line in file :
+			data = line.rstrip()
+                	decry = crypt.crypt(data,salt)
+			print ("Testando : [+] %s [+] : %s" % (data,decry))
+			if decry == hash:
+				print "Senha Encontrada : " + data
+				break
+
+main()
